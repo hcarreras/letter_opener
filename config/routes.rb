a68c7da151b8e97535c877f3375b99055c5feaf4
Hari Carreras <haricarreras@gmail.com>
2013-12-12 23:37:43 +0200

    changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get "/letter_opener"                => 'letter_opener/letters#index', :as => :letter_opener_letters
-  get "/letter_opener/:id/:style.html" => 'letter_opener/letters#show', :as => :letter_opener_letter
+  get "/letter_opener/:id" => 'letter_opener/letters#show', :as => :letter_opener_letter
 end
