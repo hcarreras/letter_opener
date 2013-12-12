89cd5fb92110b8443b01da8680bdf0f94967d7d9
Hari Carreras <haricarreras@gmail.com>
2013-12-12 22:42:00 +0200

    deliver! method works

--- spec/letter_opener/delivery_method_spec.rb
+++ spec/letter_opener/delivery_method_spec.rb
@@ -67,4 +67,23 @@ describe LetterOpener::DeliveryMethod do
     its(:contents) { should include("Hello") }
     its(:contents) { should include("World!") }
   end
+
+
+  it "saves text into html document when deliver! is called" do
+    Launchy.should_receive(:open)
+    mail = Mail.new do
+      from    'foo@example.com'
+      to      'bar@example.com'
+      subject 'Hello'
+      body    'World!'
+    end
+
+    mail.deliver!
+
+    text = File.read(Dir["#{@location}/*/plain.html"].first)
+    text.should include("foo@example.com")
+    text.should include("bar@example.com")
+    text.should include("Hello")
+    text.should include("World!")
+  end
 end
